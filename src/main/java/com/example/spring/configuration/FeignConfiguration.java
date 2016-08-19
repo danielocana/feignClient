8e6c7136314aab35bf78b8fbd444bdd8cf2fb1de
Daniel Ocaña Heredia <Daniel Ocaa Heredia>
2016-08-19 23:49:55 +0200

    refactor feign configuration.

--- src/main/java/com/example/spring/configuration/FeignConfiguration.java
+++ src/main/java/com/example/spring/configuration/FeignConfiguration.java
@@ -1,15 +1,35 @@
 package com.example.spring.configuration;
 
 import feign.Contract;
+import feign.Logger;
+import feign.RequestInterceptor;
 import feign.jaxrs.JAXRSContract;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfiguration {
 
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(FeignConfiguration.class);
+
     @Bean
     public Contract feignContract() {
         return new JAXRSContract();
+    }
+
+    @Bean
+    @ConditionalOnClass(RequestInterceptor.class)
+    public RequestInterceptor loggerInterceptor() {
+        return (template -> logger.info("Operation: " + template.method()
+                + " | URL " + template.url()
+                + " | headers " + template.headers()
+                + " | queries" + template.queries()));
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
