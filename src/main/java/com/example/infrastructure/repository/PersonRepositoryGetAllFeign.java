76cd695a417c41d10d64dc341ec116b4f2913022
Daniel Ocaña Heredia <Daniel Ocaa Heredia>
2016-08-22 21:24:24 +0200

    list person.

--- src/main/java/com/example/infrastructure/repository/PersonRepositoryGetAllFeign.java
+++ src/main/java/com/example/infrastructure/repository/PersonRepositoryGetAllFeign.java
@@ -1,13 +1,13 @@
 package com.example.infrastructure.repository;
 
 import com.example.domain.person.Person;
+import com.example.spring.configuration.FeignConfiguration;
 import feign.Param;
 import feign.RequestLine;
 import org.springframework.cloud.netflix.feign.FeignClient;
 
 import java.util.List;
 
-@FeignClient(name = "rancheros")
 public interface PersonRepositoryGetAllFeign {
 
     @RequestLine("GET /persons?limit={limit}&offset={offset}")
