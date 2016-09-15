0f4e625ca3778959df01ebc7943a636ed3e07ebf
Daniel Ocaña Heredia <Daniel Ocaa Heredia>
2016-09-16 00:01:14 +0200

    looger.

--- src/main/java/com/example/service/FindByIdPersonUseCase.java
+++ src/main/java/com/example/service/FindByIdPersonUseCase.java
@@ -2,17 +2,20 @@ package com.example.service;
 
 import com.example.domain.person.Person;
 import com.example.infrastructure.repository.PersonRepositoryFeign;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import rx.Observable;
 
-import javax.inject.Inject;
 
 public class FindByIdPersonUseCase {
 
+    private static final Logger logger = LoggerFactory.getLogger(FindByIdPersonUseCase.class);
     @Autowired
     PersonRepositoryFeign repository;
 
     public Observable<Person> findById (String id){
+        logger.info("Try to find by id = {}.", id);
         return repository.findById(id);
     }
 }
